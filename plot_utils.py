10d28640181475e0c4e2ca1a9f7f7ad69f5703a9
Brandon Clark <bclark5@zagmail.gonzaga.edu>
2021-04-18 01:07:51 +0000

    Up to Date

--- plot_utils.py
+++ plot_utils.py
@@ -1,3 +1,20 @@
 import matplotlib.pyplot as plt
 import importlib
 import numpy as np
+
+def create_histogram(name,data):
+    """ The purpose of this function is to create a histogram
+    Attributes:
+        - name(string): name of chart
+        - data(list): list of values to create histogram based off of
+    Returns:
+        - Nothing
+    """
+    # data is a 1D list of data values
+    plt.figure()
+    plt.hist(data, bins=10) # default is 10
+    plt.title("{} Histrogram".format(name))
+    plt.xlabel("{} Values".format(name))
+    plt.ylabel("Counts")
+    plt.show()
+
